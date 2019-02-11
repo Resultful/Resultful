b728c6c0404685667ff309e0cfe2f739acd3bef3
Ryan Kelly <94ryan.kelly@gmail.com>
2019-02-11 17:44:25 +0000

    More Async changes like the last commit, but for Option<T>

--- Resultful/Async/OptionExtensions.Async.cs
+++ Resultful/Async/OptionExtensions.Async.cs
@@ -25,14 +25,14 @@ namespace Resultful
                 .WrapAsync(value => value.Unroll());
 
         //BindAsync on Option<T>
-        public static Task<Option<TResult>> BindAsync<T, TResult>(this Task<Option<T>> value, Func<T, Option<TResult>> bindFunc)
+        public static Task<Option<TResult>> Bind<T, TResult>(this Task<Option<T>> value, Func<T, Option<TResult>> bindFunc)
             => value.WrapAsync(item => item.Bind(bindFunc));
 
         public static Task<Option<TResult>> BindAsync<T, TResult>(this Task<Option<T>> value, Func<T, Task<Option<TResult>>> bindFunc)
             => value.WrapAsync(item => item.BindAsync(bindFunc));
 
         //MapAsync on Option<T>
-        public static Task<Option<TResult>> MapAsync<T, TResult>(this Task<Option<T>> value, Func<T, TResult> bindFunc)
+        public static Task<Option<TResult>> Map<T, TResult>(this Task<Option<T>> value, Func<T, TResult> bindFunc)
             => value.WrapAsync(item2 => item2.Map(bindFunc));
 
         public static Task<Option<TResult>> MapAsync<T, TResult>(this Task<Option<T>> value, Func<T, Task<TResult>> bindFunc)
@@ -42,30 +42,30 @@ namespace Resultful
         public static Task<Option<T>> TeeAsync<T>(this Task<Option<T>> value, Func<T, Task> asyncFunc)
             => value.WrapAsync(item => item.TeeAsync(asyncFunc));
 
-        public static Task<Option<T>> TeeAsync<T>(this Task<Option<T>> value, Action<T> teeAction)
+        public static Task<Option<T>> Tee<T>(this Task<Option<T>> value, Action<T> teeAction)
             => value.WrapAsync(item => item.Tee(teeAction));
 
         //OrAsync on Option<T>
 
-        public static Task<T> OrAsync<T>(this Task<Option<T>> value, T otherValue)
+        public static Task<T> Or<T>(this Task<Option<T>> value, T otherValue)
             => value.WrapAsync(item => item.Or(otherValue));
 
         public static Task<T> OrAsync<T>(this Task<Option<T>> value, Task<T> otherValue)
             => value.WrapAsync(item => item.OrAsync(otherValue));
 
-        public static Task<T> OrAsync<T>(this Task<Option<T>> value, Func<T> otherFunc)
+        public static Task<T> Or<T>(this Task<Option<T>> value, Func<T> otherFunc)
             => value.WrapAsync(item => item.Or(otherFunc));
 
         public static Task<T> OrAsync<T>(this Task<Option<T>> value, Func<Task<T>> otherFunc)
             => value.WrapAsync(item => item.OrAsync(otherFunc));
 
-        public static Task<Option<T>> OrAsync<T>(this Task<Option<T>> value, Option<T> other)
+        public static Task<Option<T>> Or<T>(this Task<Option<T>> value, Option<T> other)
             => value.WrapAsync(item => item.Or(other));
 
         public static Task<Option<T>> OrAsync<T>(this Task<Option<T>> value, Task<Option<T>> other)
             => value.WrapAsync(item => item.OrAsync(other));
 
-        public static Task<Option<T>> OrAsync<T>(this Task<Option<T>> value, Func<Option<T>> otherFunc)
+        public static Task<Option<T>> Or<T>(this Task<Option<T>> value, Func<Option<T>> otherFunc)
             => value.WrapAsync(item => item.Or(otherFunc));
 
         public static Task<Option<T>> OrAsync<T>(this Task<Option<T>> value, Func<Task<Option<T>>> otherFunc)
