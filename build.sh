4340928db5a5b352ad86b35b474d607d87109e4e
Ryan Kelly <94ryan.kelly@gmail.com>
2019-03-05 08:36:06 +0000

    Installing paket globally

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 dotnet tool install fake-cli --version 5.11.1 -g
+dotnet tool install paket -g --version=5.198.0
 export PATH="$HOME/.dotnet/tools:$PATH"
 fake build  -t Publish
