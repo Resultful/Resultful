735e502729b981efa51cd12e6565b04c53c00fbf
Ryan Kelly <94ryan.kelly@gmail.com>
2021-02-27 21:42:51 +0000

    Github - Move to github

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-dotnet tool install fake-cli -g --version=5.12.4
-dotnet tool install paket -g --version=5.198.0
-export PATH="$HOME/.dotnet/tools:$PATH"
-fake build  -t Publish
+dotnet tool restore
+dotnet fake build -t Publish
